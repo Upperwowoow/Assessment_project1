765312c4ed73248df1d1dd9b1c0782a9a3c7fbc9
Upperwowoow <125507589+Upperwowoow@users.noreply.github.com>
2023-04-08 18:38:51 +1200

    playercontrol impove

--- Assessment_1/Assets/Scripts/PlayerControl.cs
+++ Assessment_1/Assets/Scripts/PlayerControl.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
 
-    public float jumpForce = 10;
+    public float jumpForce = 2;
     private Rigidbody playerRb;
     public bool isOnGround = true;
+    public Vector3 jump;
+    // i have added the variables to fix the problems 
+    public float speed = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,14 +19,32 @@ public class PlayerControl : MonoBehaviour
         playerRb = GetComponent<Rigidbody>();
     }
 
+    // I have deleted the  void OnCollisionStay() {isGround = true;} to fix the problem
+    // but it doesn't fix the problem that the player fly like rocket. 
+
+
     // Update is called once per frame
     void Update()
     {
         //player game play (Jumping)
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround);
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) ;
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+       
+        // play control moving
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
 
+    // on the ground 
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
         }
     }
 }
+
