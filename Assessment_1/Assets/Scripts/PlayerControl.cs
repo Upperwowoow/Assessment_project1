761d121b1b9fdd431b6a12f413a46285caeb7197
Upperwowoow <125507589+Upperwowoow@users.noreply.github.com>
2023-04-10 22:41:11 +1200

    Big change

--- Assessment_1/Assets/Scripts/PlayerControl.cs
+++ Assessment_1/Assets/Scripts/PlayerControl.cs
@@ -5,21 +5,20 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     //player jump
-    public float jumpForce = 2;
+    public float jumpSpeed = 8f;
+    private float direction = 0f;
 
     //player rigidbody
-    private Rigidbody playerRb;
+    private Rigidbody2D player;
 
     //on ground 
-    public bool isOnGround = true;
+    
     
     //player rotate
     public float horizontalInput;
     public float turnSpeed = 5f;
-    //player movement
-    private float forwardInput;
-    // i have added the variables to fix the problems 
-    public float speed = 10f;
+    // i have added the variables to fix the problems
+    public float Speed = 10f;
     //player gravity 
     public float gravityModifier = 1.5f;
 
@@ -27,7 +26,7 @@ public class PlayerControl : MonoBehaviour
     void Start()
     {
         //I have added playerRb = GetComponent<Rigidbody>(); in the void start to fix the problem 
-        playerRb = GetComponent<Rigidbody>();
+        player = GetComponent<Rigidbody2D>();
 
         //player gravity/physics
         Physics.gravity *= gravityModifier;
@@ -41,28 +40,43 @@ public class PlayerControl : MonoBehaviour
     void Update()
     {
         //player game play (Jumping)
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround);
+        // I have change if (Input.GetKeyDown("jump")) to if (Input.GetKeyDown(KeyCode.Space)) 
+        // issus fixed 
+        if (Input.GetKeyDown(KeyCode.Space))  
         {
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isOnGround = false;
+            player.velocity = new Vector2(player.velocity.x, jumpSpeed);
+            
         }
         // player control movement (left & righ)
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        direction = Input.GetAxis("Horizontal");
+
+        if (direction > 0f)
+        {
+            player.velocity = new Vector2(direction * Speed, player.velocity.y);
+        }
+
+        else if (direction < 0f)
+        {
+            //I have spell velocity wrong 
+            player.velocity = new Vector2(direction * Speed, player.velocity.y);
+        }
+
+        else  
+        {
+            player.velocity = new Vector2(0 * Speed, player.velocity.y);
+        }
+
+        // I delete transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //I have delete most of the thing that is player movement 
+        //I have also change vector3 to vector2 also Rigidbody3D to Rigidbody2D
 
         //player rotate 
         horizontalInput = Input.GetAxis("Horizontal");
         //I have fixed the issue by adding , to up - issuse fix
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        transform.Rotate(Vector2.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 
     // on the ground 
-    private void OnCollisionEnter(Collision collision)
-    {
-
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isOnGround = true;
-        }
-    }
+   
 }
 
